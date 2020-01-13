34742f57bd8a52b0d461c929964e260d8f248335
marwa <youn0549@algonquinlive.com>
2020-01-13 15:37:46 -0500

    participation 2

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+//  this is a single line comment
+
+
+
+let name = 'MARWA'
+console.log(`Hello, name is ${name}`)
+
+
+let number = 5
+
+let product = number * 2
+console.log(`The number ${number} multiplied by 2 is ${number * 2}.`)
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
+
